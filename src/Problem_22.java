8de12ce5593299546170a9fc010a3083a26bd00c
aditya1518 <adityasinghpatel786@gmail.com>
2025-02-09 23:39:39 +0530

    Commit

--- src/Problem_22.java
+++ src/Problem_22.java
@@ -3,28 +3,31 @@ public class Problem_22{
 	public static void main(String[] args) {
 	    Scanner sc= new Scanner(System.in);
 	    System.out.print("Enter the number: ");
-	    int n=sc.nextInt();
-        // for Uper Part
-	    for(int i=0;i<n;i++){
-            for (int j =n-i; j>=0 ; j--){
-                System.out.print(" ");
-            }
-            for(int j=0;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        // Lower Part
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<=i-1;j++){
-                System.out.print(" ");
-             }
-            for(int j=(n-1)-i;j>0;j--){
-                System.out.print("* ");
-            }
-             
-            System.out.println();
-        }
-        
+	    int n =sc.nextInt();
+	   // Upper Part
+	    for(int i=1;i<=n;i++){
+	        for(int j =0;j<=n;j++){
+	           if(j<=n-i){
+	               System.out.print(" ");
+	           }
+	           else{
+	               System.out.print("* ");
+	           }
+	        }
+	        System.out.println();
+	    }
+	   // Lower Part
+	    for(int i=1;i<n;i++){
+	        for(int j=n;j>=0;j--){
+	            if(j>=n-i){
+	                System.out.print(" ");
+	            }
+	            else{
+	                System.out.print("* ");
+	            }
+	        }
+	        System.out.println();
+	    }
 	}
+	
 }
